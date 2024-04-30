d1f1574d67b08448706fc047259b9036f7a55266
berni.niko <berni.niko@hotmail.com>
2024-04-30 23:26:56 +1200

    update

--- scripts/temple.js
+++ scripts/temple.js
@@ -1,9 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.querySelector('.hamburger');
-    const navUL = document.querySelector('nav ul');
-
-    hamburger.addEventListener('click', () => {
-        navUL.classList.toggle('open');
+    const navMenu = document.querySelector('nav ul');
+    hamburger.addEventListener('click', function() {
+        // This toggles the display of the nav menu between block and none
+        navMenu.style.display = navMenu.style.display === 'block' ? 'none' : 'block';
     });
+    // Populate the current year
+    var currentYearSpan = document.getElementById("currentyear");
+    currentYearSpan.textContent = new Date().getFullYear(); // Set current year
+    // Function to get current date and time in a specific format
+    function currentDate() {
+        var date = new Date();
+        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+        return date.toLocaleDateString(undefined, options);
+    }
+    // Function to update date and time in footer
+    function updateDateTimeFooter() {
+        var footerDateTimeSpan = document.getElementById("footerDateTime");
+        footerDateTimeSpan.textContent = 'Last Modifications ' + currentDate(); // Update date and time in footer
+    }
+    // Call the function to update the date and time when the page loads
+    updateDateTimeFooter();
+    // Update the date and time every second
+    setInterval(updateDateTimeFooter, 1000);
 });
 
